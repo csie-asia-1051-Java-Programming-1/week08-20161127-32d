49d11d28cda0b03c8ad40742c49586b905749b71
32d <105021055@live.asia.edu.tw>
2016-11-21 16:02:01 +0800

    0

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -1,15 +1,24 @@
-﻿package ex;
+﻿
+package ex;
 /*
  * Topic: 請寫一個函式 (不是遞迴) 進行十進制數字轉換成二進制數字，例如十進制的 13 會輸出二進制的 1101
  * Date: 2016/11/21
  * Author: 1050210XX 周永振老師
  */
-
+import java.util.Scanner;
 public class ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int a = 0;
+		while (n>0){
+		a = n%2;
+		n = n/2;		
+		String str = new StringBuilder(a).reverse().toString();
+		System.out.print(str);
+		}
+		
 	}
-
 }
